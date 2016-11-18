71bae0da32bf106bc6b6fe1e1737f3caa5351fb8
Luis Rodriguez <luis_rdz5@hotmail.com>
2016-11-18 17:50:40 -0600

    se agrego sql que inserta en agenda de gestion creditos en buro de credito

---
+++ insertarnotificaciónagendagestion creditos en buro de credito.sql
@@ -0,0 +1,46 @@
+INSERT INTO [SIRCANAC].[dbo].[gestioninterna]
+           ([rpu]
+           ,[fechagestion]
+           ,[tipogestion]
+           ,[diasvencido]
+           ,[importevencido]
+           ,[fechapromesa]
+           ,[resp1]
+           ,[resp2]
+           ,[resp2otra]
+           ,[resp3]
+           ,[resp4]
+           ,[comentario]
+           ,[moratorios]
+           ,[iva]
+           ,[gestor]
+           ,[NumCredito])
+select 
+cr.rpu
+, buro.FechaAlta
+,'7'
+,(select isnull(datediff(dd,MIN(fecha),getdate()),0)  from det_amortizacion  where estatus not in ('14','18')and rpu=cr.rpu) 
+,(select isnull(SUM(pagodec),0)  from det_amortizacion  where estatus not in ('14','18')and rpu=cr.rpu)
+,NULL
+,''
+,''
+,''
+,''
+,''
+,'ENVIADO CON EL VISTO BUENO DE LA ZONA'
+,0
+,0
+,'FINANZAS'
+, cr.numcredito
+from 
+(select *
+from muestraBuro 
+union
+select *
+from MuestraBuro_PM ) as buro
+join creditos cr on cr.NumCredito=buro.numcredito
+where cr.rpu not in (
+select rpu 
+from gestioninterna
+where tipogestion='7'
+) and buro.FechaAlta is not null
