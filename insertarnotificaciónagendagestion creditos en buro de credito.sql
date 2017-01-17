f43e4c08935cca8f2d7621e8833da66dfa0cf2c9
Luis Rodriguez <luis_rdz5@hotmail.com>
2017-01-17 11:44:34 -0600

    Se agregaron los querys para quebrantar creditos y para obtener fechas de cortes mensuales

--- insertarnotificaciónagendagestion creditos en buro de credito.sql
+++ insertarnotificaciónagendagestion creditos en buro de credito.sql
@@ -44,3 +44,26 @@ select rpu
 from gestioninterna
 where tipogestion='7'
 ) and buro.FechaAlta is not null
+
+
+
+-- Posteriormente se debe actualizar los clientes
+
+
+update clientes
+set EstatusGestion='7'
+where rpu in (
+select 
+cr.rpu
+from 
+(select *
+from muestraBuro 
+union
+select *
+from MuestraBuro_PM ) as buro
+join creditos cr on cr.NumCredito=buro.numcredito
+where cr.rpu not in (
+select rpu 
+from clientes
+where EstatusGestion='7'
+) and buro.FechaAlta is not null)
---
+++ update para quebrantar credito en sirca.sql
@@ -0,0 +1,1 @@
+update creditos set estatusCredito='20', actaQuebranto='ACUERDO 83.05', fechaquebranto='20161128' , capitalquebrantado=' 2633.14' where numsolicitud='91298370'
